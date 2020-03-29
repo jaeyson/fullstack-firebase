b501b4dcb5e751507b9d9562e7cf132953ea471c
jaeyson <hello@jaeyson.ml>
2020-03-29 10:39:31 +0700

    update auth.js

--- public/src/auth.js
+++ public/src/auth.js
@@ -1,18 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
+  const getID = x => document.getElementById(x);
   const auth = firebase.auth();
   const db = firebase.firestore();
-  const signupForm = document.querySelector('#signup-form');
 
-  signupForm.addEventListener('submit', (e) => {
+  getID("signup-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const email = signupForm['register-email'].value;
-    const password = signupForm['register-password'].value;
+    const email = signupForm["register-email"].value;
+    const password = signupForm["register-password"].value;
 
     auth.createUserWithEmailAndPassword(email, password)
-      .then(() => {
+      .then({
+        getID("modal-register").classList.replace("block", "hidden");
         signupForm.reset();
-        document.getElementById('modal-register')
-          .classList.replace("block", "hidden");
-      })
-  })
-})
+      });
+  });
+
+  getID("logout").addEventListener("click", (e) => {
+    e.preventDefault();
+    auth.signOut().then(console.log("logged out"));
+  });
+});
+
