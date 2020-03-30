1b424673f814246f34c94fa476d545b9ca887d4d
jaeyson <hello@jaeyson.ml>
2020-03-30 11:19:49 +0700

    fetch data from firestore

--- public/src/auth.js
+++ public/src/auth.js
@@ -1,23 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
   const getID = x => document.getElementById(x);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
+  //const auth = firebase.auth();
+  //const db = firebase.firestore();
+  const errorSwitch = err => ({
+    "auth/wrong-password": "wrong password",
+    "auth/invalid-email": "wrong email",
+    "auth/user-disabled": "user has been disabled",
+    "auth/user-not-found": "user not found",
+    "auth/network-request-failed": "A network error (such as timeout, interrupted connection or unreachable host) has occurred."
+  })[err]; //errorSwitch(errorCode);
+  //const getOptions = { source: "cache" };
 
-  getID("signup-form").addEventListener("submit", (e) => {
+  firebase.firestore().collection("guides").get().then(snapshot => setupGuides(snapshot.docs));
+
+  firebase.auth().onAuthStateChanged(user => user ? console.log("logged in") : console.log("logged out"));
+
+  getID("register-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const email = signupForm["register-email"].value;
-    const password = signupForm["register-password"].value;
+    const email = getID("register-form")["register-email"].value;
+    const password = getID("register-form")["register-password"].value;
 
-    auth.createUserWithEmailAndPassword(email, password)
-      .then({
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(() => {
         getID("modal-register").classList.replace("block", "hidden");
-        signupForm.reset();
+        getID("register-form").reset();
       });
+  });
+
+  getID("login-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = getID("login-form")["login-email"].value;
+    const password = getID("login-form")["login-password"].value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      //.catch(error => console.log(errorSwitch(error.code)));
+      .then(() => {
+        getID("modal-login").classList.replace("block", "hidden");
+        getID("login-form").reset();
+      })
   });
 
   getID("logout").addEventListener("click", (e) => {
     e.preventDefault();
-    auth.signOut().then(console.log("logged out"));
+    firebase.auth().signOut();
   });
+
 });
 
--- public/src/main.js
+++ public/src/main.js
@@ -1,5 +1,23 @@
 const modalTrigger = document.querySelectorAll('.modal-trigger');
+const guideList = document.querySelector(".guides");
 
+const setupGuides = data => {
+  let html = "";
+  data.forEach(doc => {
+    const guide = doc.data();
+    const li = `
+      <details>
+        <summary>${guide.title}</summary>
+        <p>${guide.content}</p>
+      </details>
+    `;
+    html += li;
+  })
+
+  guideList.innerHTML = html;
+};
+
+//modal
 modalTrigger.forEach(trigger => {
   trigger.addEventListener('click', (e) => {
     //const modal = document.querySelector(`#modal-${trigger.id}`);
