eb5fa0c1d5065e9ce2bb40a9883b26a76e0ad82b
jaeyson <hello@jaeyson.ml>
2020-03-31 16:59:52 +0800

    update fetch data from firestore

--- public/src/auth.js
+++ public/src/auth.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const getID = x => document.getElementById(x);
-  //const auth = firebase.auth();
-  //const db = firebase.firestore();
+  const auth = firebase.auth();
+  const db = firebase.firestore();
   const errorSwitch = err => ({
     "auth/wrong-password": "wrong password",
     "auth/invalid-email": "wrong email",
@@ -9,41 +9,32 @@ document.addEventListener("DOMContentLoaded", function() {
     "auth/user-not-found": "user not found",
     "auth/network-request-failed": "A network error (such as timeout, interrupted connection or unreachable host) has occurred."
   })[err]; //errorSwitch(errorCode);
-  //const getOptions = { source: "cache" };
 
-  firebase.firestore().collection("guides").get().then(snapshot => setupGuides(snapshot.docs));
+  let getOrSetInput = (event, method, fn) => {
+    event.preventDefault();
+    const email = getID(`${method}-form`)[`${method}-email`].value;
+    const password = getID(`${method}-form`)[`${method}-password`].value;
 
-  firebase.auth().onAuthStateChanged(user => user ? console.log("logged in") : console.log("logged out"));
-
-  getID("register-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    const email = getID("register-form")["register-email"].value;
-    const password = getID("register-form")["register-password"].value;
-
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(() => {
-        getID("modal-register").classList.replace("block", "hidden");
-        getID("register-form").reset();
-      });
-  });
-
-  getID("login-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    const email = getID("login-form")["login-email"].value;
-    const password = getID("login-form")["login-password"].value;
-
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    auth.fn(email, password).then(() => {
       //.catch(error => console.log(errorSwitch(error.code)));
-      .then(() => {
-        getID("modal-login").classList.replace("block", "hidden");
-        getID("login-form").reset();
-      })
-  });
+      getID(`modal-${method}`).classList.replace("block", "hidden");
+      getID(`${method}-form`).reset();
+    })
+  };
+  let getOptions = { source: "cache" };
 
-  getID("logout").addEventListener("click", (e) => {
-    e.preventDefault();
-    firebase.auth().signOut();
-  });
+  db.collection("guides").get().then(snapshot => setupGuides(".guides", snapshot.docs));
+
+  auth.onAuthStateChanged(usr => usr ? console.log("logged in") : console.log("logged out"));
+
+  getID("register-form")
+    .addEventListener("submit", e => getOrSetInput(e, register, createUserWithEmailAndPassword));
+
+  getID("login-form")
+    .addEventListener("submit", e => getOrSetInput(e, login, signInWithEmailAndPassword));
+
+  getID("logout")
+    .addEventListener("click", e => {e.preventDefault();auth.signOut()});
 
 });
 
--- public/src/main.js
+++ public/src/main.js
@@ -1,31 +1,23 @@
-const modalTrigger = document.querySelectorAll('.modal-trigger');
-const guideList = document.querySelector(".guides");
-
-const setupGuides = data => {
-  let html = "";
-  data.forEach(doc => {
-    const guide = doc.data();
-    const li = `
-      <details>
-        <summary>${guide.title}</summary>
-        <p>${guide.content}</p>
-      </details>
-    `;
-    html += li;
-  })
-
-  guideList.innerHTML = html;
-};
-
-//modal
-modalTrigger.forEach(trigger => {
-  trigger.addEventListener('click', (e) => {
-    //const modal = document.querySelector(`#modal-${trigger.id}`);
-    const modals = document.getElementsByClassName('modal');
-    Array.from(modals).forEach(modal => {
+document.querySelectorAll('.modal-trigger').forEach(trigger => {
+  trigger.addEventListener('click', e => {
+    Array.from(document.getElementsByClassName("modal"))
+    .forEach(modal => {
       (modal.id == `modal-${trigger.id}`)
         ? modal.classList.replace("hidden", "block")
         : modal.classList.replace("block", "hidden")
     })
   })
 })
+
+const setupGuides = (elem, data, html="") => {
+  data.forEach(doc => {
+    const li = `
+      <details>
+        <summary>${doc.data().title}</summary>
+        <p>${doc.data().content}</p>
+      </details>`;
+    html += li;
+  })
+  document.querySelector(elem).innerHTML = html;
+};
+
